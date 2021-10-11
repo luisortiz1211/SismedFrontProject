2453b0217dfc066d24cd6888c21188f64cd1f4ed
luisortiz1211 <luis.ortiz02@epn.edu.ec>
2021-10-11 01:29:51 -0500

    patients withAuth

--- src/pages/patients/index.js
+++ src/pages/patients/index.js
@@ -3,6 +3,7 @@ import ChargeInformation from "@/components/ChargeInformation";
 import LayoutSecondary from "@/components/LayoutSecondary";
 import Loading from "@/components/Loading";
 import Title from "@/components/Title";
+import withAuth from "@/hocs/withAuth";
 import { Button, Grid } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
@@ -327,4 +328,4 @@ const PatientsList = () => {
     </LayoutSecondary>
   );
 };
-export default PatientsList;
+export default withAuth(PatientsList);
